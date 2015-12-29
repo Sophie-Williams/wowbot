760553001758fb778be30d5c6210e005d41467eb
Ralin Chimev <ralin.chimev@gmail.com>
2015-12-29 11:50:57 +0200

    Various improvements

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# GameBot
+# WoW game bot
+
+Used for disenchanting a lot of items without manually pressing the macro key.
+
+```
+Example: 
+
+Given that you have the following disenchanting macro in game: 
+
+/cast disenchant
+/use item:54473
+
+bound to F1 key. Then run the PresKey.exe like this: 
+
+> PressKey.exe F1 100
+
+where "F1" is the function key to which the macro is bound and "100" is the number of items you need to disenchant. 
+
+If you run it without any command line arguements, then the default key is F1 and the default number of items is 100.
+
+Note: Use only keys from F1 to F12!
+
+```
--- main.cpp
+++ main.cpp
@@ -3,39 +3,62 @@
 
 #include "stdafx.h"
 
-#define SECOND 1000
-#define KEY_CODE_F2 VK_F2
-#define SCAN_CODE_F2 0x32
-#define DEFAULT_ITERATIONS 100
+void wait(int minWaitInSeconds, int maxWaitInSeconds) 
+{
+	const int ONE_SECOND_IN_MILLIS = 1000;
 
-void wait(int minWaitInSeconds, int maxWaitInSeconds) {
-	int randomWaitTime = (minWaitInSeconds * SECOND) + (rand() % ((maxWaitInSeconds - minWaitInSeconds) * SECOND) + 1);
-	printf("Waiting %i milliseconds ...\r\n", randomWaitTime);
-	::Sleep(randomWaitTime);
+	int minWaitTime = (minWaitInSeconds * ONE_SECOND_IN_MILLIS);
+	int randomWaitTime = (rand() % ((maxWaitInSeconds - minWaitInSeconds) * ONE_SECOND_IN_MILLIS));
+	int waitTime = (minWaitTime + randomWaitTime);
+
+	printf("Waiting %i milliseconds ...\r\n", waitTime);
+
+	Sleep(waitTime);
 }
 
-void pressKey(int keyCode, int scanCode) {
-	keybd_event(keyCode, scanCode, KEYEVENTF_EXTENDEDKEY | 0, 0);
-	keybd_event(keyCode, scanCode, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+void pressKey(int keyCode) 
+{
+	UINT scanCode = MapVirtualKeyEx(keyCode, MAPVK_VK_TO_VSC, NULL);
+
+	INPUT input;
+	input.type = INPUT_KEYBOARD;
+	input.ki.wScan = scanCode;
+	input.ki.time = 0;
+	input.ki.dwExtraInfo = 0;
+	input.ki.wVk = keyCode;
+	input.ki.dwFlags = KEYEVENTF_EXTENDEDKEY | 0;
+
+	SendInput(1, &input, sizeof(INPUT));
+
+	input.ki.dwFlags = KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP;
+
+	SendInput(1, &input, sizeof(INPUT));
 }
 
-void pressF2AndWait() {
-	pressKey(KEY_CODE_F2, SCAN_CODE_F2);
-	wait(2, 4);
-}
+int _tmain(int argc, _TCHAR* argv[]) 
+{
+	const int DEFAULT_EXECUTION_COUNT = 100;
+	const int DEFAULT_KEY = VK_F1;
 
-int _tmain(int argc, _TCHAR* argv[]) {
-	int times = DEFAULT_ITERATIONS;
+	int times = DEFAULT_EXECUTION_COUNT;
+	int key = DEFAULT_KEY;
 
-	if (argc > 1) {
-		times = _ttoi(argv[1]);
+	if (argc > 1) 
+	{
+		key = VK_F1 + (_ttoi(&argv[1][1]) - 1);
+	}
+
+	if (argc > 2) 
+	{
+		times = _ttoi(argv[2]);
 	}
 
 	printf("Executing %i iterations...\r\n", times);
 
 	for (int i = 0; i < times; i++) {
 		printf("Executing iteration %i\r\n", (i + 1));
-		pressF2AndWait();
+		pressKey(key);
+		wait(2, 4);
 	}
 
 	printf("Done.");
